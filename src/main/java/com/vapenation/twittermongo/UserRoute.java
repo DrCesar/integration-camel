33961b5f9647902352ee128183505826985def2b
JosueBunker <josue@bunkeraccess.com>
2019-11-15 22:15:27 -0600

    Added Kafka

--- src/main/java/com/vapenation/twittermongo/UserRoute.java
+++ src/main/java/com/vapenation/twittermongo/UserRoute.java
@@ -1,11 +1,18 @@
 package com.vapenation.twittermongo;
 
+
+import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.Exchange;
+
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Component
 public class UserRoute extends RouteBuilder {
+
+    @Autowired
+    CamelContext context;
 
     @Override
     public void configure() throws Exception {
